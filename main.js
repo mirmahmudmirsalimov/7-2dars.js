29b2fa491638c0ffe7e45873d2e0c8ac41af5008
mirmahmud2007 <mirmahmudmirsali@gmail.com>
2022-07-07 19:21:41 +0500

    pers

---
+++ main.js
@@ -0,0 +1,5 @@
+function moneyConverter(valNum) {
+    document.getElementById("outputrub").innerHTML=valNum*10860;
+    document.getElementById("outputrub1").innerHTML=valNum*11074;
+    document.getElementById("outputrub2").innerHTML=valNum*60;
+}
